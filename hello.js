80509341b384352d40f31d6d154cc33470733a00
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-07 10:09:49 +0200

    lesson24 The Event Loop

--- hello.js
+++ hello.js
@@ -1,6 +1,32 @@
-function findMeaning() {
-	return 42
-}
+////// // lesson10
 
-console.log(findMeaning())
-object.assign()
+// function findMeaning() {
+// 	return 42
+// }
+
+// console.log(findMeaning())
+// object.assign()
+
+////// // lesson11
+
+// const mission = 'learn'
+
+// if (mission === 'learn') {
+// 	console.log('Time to write some Node code!')
+// } else {
+// 	console.log(`Js ${mission} really more fun?`)
+// }
+
+// const mission = process.argv[2]
+
+// if (mission === 'learn') {
+// 	console.log('Time to write some Node code!')
+// } else {
+// 	console.log(`Js ${mission} really more fun?`)
+// }
+
+////// // lesson24 The Event Loop
+
+while (!shouldExit) {
+	processEvents()
+}
