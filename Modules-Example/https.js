4c5dc78ca52f25e440603c22a0506b12a22cfee2
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-08 13:20:09 +0200

    Node Package Manager

--- Modules-Example/https.js
+++ Modules-Example/https.js
@@ -1,9 +1,6 @@
-const { send, read } = require('./internals')
-
-// const { send } = require('./internals/request')
-// const { read } = require('./internals/response')
-
-// const { REQUEST_TIMEOUT } = require('./request')
+const { send } = require('./internals/request')
+const { read } = require('./internals/response')
+const { REQUEST_TIMEOUT } = require('./request')
 
 function makeRequest(url, data) {
 	send(url, data)
@@ -13,4 +10,4 @@ function makeRequest(url, data) {
 const responseData = makeRequest('https://google.com')
 console.log(responseData)
 
-// console.log(require.cache)
+console.log(require.cache)
