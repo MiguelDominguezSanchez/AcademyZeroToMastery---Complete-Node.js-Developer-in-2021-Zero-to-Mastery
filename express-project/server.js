9f217c85a0f506b529c9ab34e9dd45d42f7cc58c
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-11 18:28:37 +0200

    lesson 81

--- express-project/server.js
+++ express-project/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const { restart } = require('nodemon')
 
-const friendsController = require('./controllers/friends.controller')
-const messagesController = require('./controllers/messages.controller')
+const friendsRouter = require('./routes/friends.router')
+const messagesRouter = require('./routes/messages.router')
 
 const app = express()
 
@@ -13,17 +13,13 @@ app.use((req, res, next) => {
 	next()
 	//actions go here...
 	const delta = Date.now() - start
-	console.log(`${req.method} ${req.url} ${delta}ms`)
+	console.log(`${req.method} ${req.baseurl}${req.url} ${delta}ms`)
 })
 
 app.use(express.json())
 
-app.post('/friends', friendsController.postFriend)
-app.get('/friends', friendsController.getFriends)
-app.get('/friends/:friendId', friendsController.getFriend)
-
-app.get('/messages', messagesController.getMessages)
-app.post('/messages', messagesController.postMessage)
+app.use('/friends', friendsRouter)
+app.use('/messages', messagesRouter)
 
 app.listen(PORT, () => {
 	console.log(`Listening on ${PORT}...`)
