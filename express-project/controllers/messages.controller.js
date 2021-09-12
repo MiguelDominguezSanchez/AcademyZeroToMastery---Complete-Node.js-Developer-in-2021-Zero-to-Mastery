45a1ad84f700e8c9186ebc0d589fec03182da366
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-12 09:07:13 +0200

    lesson 84

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,5 +1,9 @@
+const path = require('path')
+// /folders/files.jpg /folder/files.jpg
+
 function getMessages(req, res) {
-	res.send('<ul><li>Hello Albert!</li></ul>')
+	res.sendFile(path.join(__dirname, '..', 'public', 'rioparmountain.jpg'))
+	// res.send('<ul><li>Hello Albert!</li></ul>')
 }
 
 function postMessage(req, res) {
