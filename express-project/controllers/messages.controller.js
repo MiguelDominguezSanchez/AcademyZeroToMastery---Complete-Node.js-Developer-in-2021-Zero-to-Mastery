e7e46dd26b76cd1d27b8cb6fe776a51f3bc3687c
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-12 09:36:37 +0200

    lesson 85

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -2,8 +2,9 @@ const path = require('path')
 // /folders/files.jpg /folder/files.jpg
 
 function getMessages(req, res) {
-	res.sendFile(path.join(__dirname, '..', 'public', 'rioparmountain.jpg'))
-	// res.send('<ul><li>Hello Albert!</li></ul>')
+	res.sendFile(
+		path.join(__dirname, '..', 'public', 'images', 'rioparmountain.jpg')
+	)
 }
 
 function postMessage(req, res) {
--- express-project/server.js
+++ express-project/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { restart } = require('nodemon')
+const path = require('path')
 
 const friendsRouter = require('./routes/friends.router')
 const messagesRouter = require('./routes/messages.router')
@@ -16,6 +17,7 @@ app.use((req, res, next) => {
 	console.log(`${req.method} ${req.baseurl}${req.url} ${delta}ms`)
 })
 
+app.use('/site', express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 
 app.use('/friends', friendsRouter)
