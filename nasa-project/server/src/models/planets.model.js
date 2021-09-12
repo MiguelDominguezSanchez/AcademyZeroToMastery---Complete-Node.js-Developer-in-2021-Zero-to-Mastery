6e53b1f60fbe5947780388d3f6b2446dffd8e9d6
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-12 17:23:39 +0200

    lesson 98

--- nasa-project/server/src/models/planets.model.js
+++ nasa-project/server/src/models/planets.model.js
@@ -1,5 +1,6 @@
-const parse = require('csv-parse')
 const fs = require('fs')
+const path = require('path')
+const parse = require('csv-parse')
 
 const habitablePlanets = []
 
@@ -12,30 +13,45 @@ function isHabitablePlanet(planet) {
 	)
 }
 
-fs.createReadStream('kepler_data.csv')
-	.pipe(
-		parse({
-			comment: '#',
-			columns: true,
-		})
-	)
-	.on('data', (data) => {
-		if (isHabitablePlanet(data)) {
-			habitablePlanets.push(data)
-		}
-	})
-	.on('error', (err) => {
-		console.log(err)
-	})
-	.on('end', () => {
-		console.log(
-			habitablePlanets.map((planet) => {
-				return planet['kepler_name']
-			})
+/*
+const promise = new Promise((resolve, reject) => {
+  resolve(42);
+});
+promise.then((result) => {
+
+});
+const result = await promise;
+console.log(result)
+*/
+
+function loadPlanetsData() {
+	return new Promise((resolve, reject) => {
+		fs.createReadStream(
+			path.join(__dirname, '..', '..', 'data', 'kepler_data.csv')
 		)
-		console.log(`${habitablePlanets.length} habitable planets found!`)
+			.pipe(
+				parse({
+					comment: '#',
+					columns: true,
+				})
+			)
+			.on('data', (data) => {
+				if (isHabitablePlanet(data)) {
+					habitablePlanets.push(data)
+				}
+			})
+			.on('error', (err) => {
+				console.log(err)
+				reject(err)
+			})
+			.on('end', () => {
+				console.log(`${habitablePlanets.length} habitable planets found!`)
+				resolve()
+			})
 	})
+}
 
 module.exports = {
+	loadPlanetsData,
 	planets: habitablePlanets,
 }
