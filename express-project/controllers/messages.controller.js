81f6b42f1661bc607c5d9ebf55ab6d3f71cc1502
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-12 10:17:26 +0200

    lesson 87

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,10 +1,15 @@
+const { readdirSync } = require('fs')
 const path = require('path')
 // /folders/files.jpg /folder/files.jpg
 
 function getMessages(req, res) {
-	res.sendFile(
-		path.join(__dirname, '..', 'public', 'images', 'rioparmountain.jpg')
-	)
+	res.render('messages', {
+		title: 'Messages to my Friends!',
+		friend: 'Elon Musk',
+	})
+	// res.sendFile(
+	// 	path.join(__dirname, '..', 'public', 'images', 'rioparmountain.jpg')
+	// )
 }
 
 function postMessage(req, res) {
